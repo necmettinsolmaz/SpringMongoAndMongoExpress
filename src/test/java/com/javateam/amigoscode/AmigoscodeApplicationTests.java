17279cf814a3f3a7f670f0cf36d7a992cf840346
Necmettin <necmettin.solmaz@icisleri.gov.tr>
2023-12-31 22:25:28 +0300

    docker-compose ve application.properties dosyası oluşturuldu.

---
+++ src/test/java/com/javateam/amigoscode/AmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javateam.amigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
